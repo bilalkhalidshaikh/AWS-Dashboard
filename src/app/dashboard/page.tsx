6cdfb4f2ddb65f43f83c93090dabe93a6ba6bb1d
Muhammad Bilal Khalid <64368766+bilalkhalidshaikh@users.noreply.github.com>
2024-05-14 23:31:05 +0500

    removed auth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import type { Metadata } from 'next';
 import Grid from '@mui/material/Unstable_Grid2';
 import dayjs from 'dayjs';
-
 import { config } from '@/config';
 import { Budget } from '@/components/dashboard/overview/budget';
 import { LatestOrders } from '@/components/dashboard/overview/latest-orders';
